0eb6d14a960048bb78cde6ce45baba8178a8e00b
TMMorrow <morrowtym@gmail.com>
2021-05-07 10:17:41 -0500

    Animation and started the constant transform stuff.

---
+++ Assets/Scripts/TimeTransform.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTransform : MonoBehaviour
+{
+    public List<Transform> locationObject;
+    public List<float> locationTime;
+    public int currentLevel;
+
+    private void Start()
+    {
+        currentLevel = 0;
+        StartCoroutine(ChangePeriod(locationTime[0]));
+    }
+
+    private IEnumerator ChangePeriod(float sec)
+    {
+        yield return new WaitForSecondsRealtime(sec);
+        NextLevel();
+        StartCoroutine(ChangePeriod(locationTime[currentLevel]));
+    }
+
+    private void NextLevel()
+    {
+        GetComponent<CharacterController>().enabled = false;
+        StartCoroutine(ChangeAnimation());
+        currentLevel++;
+        if (currentLevel > locationObject.Count - 1)
+        {
+            currentLevel = 0;
+        }
+        transform.position = locationObject[currentLevel].position;
+    }
+
+    private IEnumerator ChangeAnimation()
+    {
+        yield return new WaitForSecondsRealtime(8);
+        GetComponent<CharacterController>().enabled = true;
+    }
+    
+}
