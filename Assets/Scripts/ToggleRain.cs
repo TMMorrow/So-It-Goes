9de1c05f650d239f10074acc1f2bfd881a459bd8
typack <morrowtym@gmail.com>
2021-05-09 21:15:37 -0500

    Almost done with New York, wasn't very focused but it's getting there. Needs new park instead of bad low poly one.

---
+++ Assets/Scripts/ToggleRain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DigitalRuby.RainMaker;
+using UnityEngine;
+
+public class ToggleRain : MonoBehaviour
+{
+    public GameObject rainPrefab;
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if (rainPrefab.GetComponent<RainScript>().RainFallParticleSystem.isPlaying)
+        {
+            rainPrefab.GetComponent<RainScript>().RainFallParticleSystem.Stop();
+        }
+        else if(rainPrefab.GetComponent<RainScript>().RainFallParticleSystem.isPaused || rainPrefab.GetComponent<RainScript>().RainFallParticleSystem.isStopped)
+        {
+            rainPrefab.GetComponent<RainScript>().RainIntensity = .4f;
+            rainPrefab.GetComponent<RainScript>().RainFallParticleSystem.Play();
+        }
+    }
+    
+}
